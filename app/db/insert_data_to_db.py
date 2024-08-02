5bf74e4b92f44151b9c11c9589af21d4febd31a4
yoonjin <yj0331yj@gmail.com>
2024-08-02 10:09:55 +0900

    테이블 데이터 삽입 오류 수정 반영

--- app/db/insert_data_to_db.py
+++ app/db/insert_data_to_db.py
@@ -13,53 +13,51 @@ def insert_sport_event_query(data: dict) -> None:
         )
 
         with connection.cursor() as cursor:
-            # SQL 문 구성
             insert_query = sql.SQL(
                 """
                 INSERT INTO sport_event (
                     id, start_time, start_time_confirmed, sport_name, category_name,
                     competition_name, competition_gender, season_name, season_start_date,
                     season_end_date, stage_order, stage_type, stage_phase, round_number,
-                    group_id, group_name, status, match_status, home_score, away_score, winner_id
-                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    group_id, group_name, status, match_status, home_score, away_score, winner_id, venue_id
+                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                 ON CONFLICT (id) DO NOTHING
                 """
             )
 
-            # competitors 데이터를 데이터베이스에 삽입
             for event in data['summaries']:
                 sport_event = event['sport_event']
                 sport_event_context = sport_event['sport_event_context']
                 sport_event_status = event['sport_event_status']
                 cursor.execute(insert_query, (
-                    sport_event['id'],
-                    sport_event['start_time'],
-                    sport_event['start_time_confirmed'],
-                    sport_event_context['sport']['name'],
-                    sport_event_context['category']['name'],
-                    sport_event_context['competition']['name'],
-                    sport_event_context['competition']['gender'],
-                    sport_event_context['season']['name'],
-                    sport_event_context['season']['start_date'],
-                    sport_event_context['season']['end_date'],
-                    sport_event_context['stage']['order'],
-                    sport_event_context['stage']['type'],
-                    sport_event_context['stage']['phase'],
-                    sport_event_context['round']['number'],
-                    sport_event_context['groups'][0]['id'],
-                    sport_event_context['groups'][0]['name'],
-                    sport_event_status['status'],
-                    sport_event_status['match_status'],
-                    sport_event_status['home_score'],
-                    sport_event_status['away_score'],
-                    sport_event_status['winner_id']
+                    sport_event.get('id'),
+                    sport_event.get('start_time'),
+                    sport_event.get('start_time_confirmed'),
+                    sport_event_context.get('sport', {}).get('name'),
+                    sport_event_context.get('category', {}).get('name'),
+                    sport_event_context.get('competition', {}).get('name'),
+                    sport_event_context.get('competition', {}).get('gender'),
+                    sport_event_context.get('season', {}).get('name'),
+                    sport_event_context.get('season', {}).get('start_date'),
+                    sport_event_context.get('season', {}).get('end_date'),
+                    sport_event_context.get('stage', {}).get('order'),
+                    sport_event_context.get('stage', {}).get('type'),
+                    sport_event_context.get('stage', {}).get('phase'),
+                    sport_event_context.get('round', {}).get('number'),
+                    sport_event_context.get('groups', [{}])[0].get('id'),
+                    sport_event_context.get('groups', [{}])[0].get('name'),
+                    sport_event_status.get('status'),
+                    sport_event_status.get('match_status'),
+                    sport_event_status.get('home_score'),
+                    sport_event_status.get('away_score'),
+                    sport_event_status.get('winner_id'),
+                    sport_event.get('venue_id')
                 ))
 
             connection.commit()
 
-
     except (Exception, psycopg2.Error) as error:
-        print("Error while inserting to PostgreSQL", error)
+        print("Error while inserting sport_event to PostgreSQL:", error)
     finally:
         if connection:
             connection.close()
@@ -76,7 +74,6 @@ def insert_competitors_query(data: dict) -> None:
         )
 
         with connection.cursor() as cursor:
-            # SQL 문 구성
             insert_query = sql.SQL(
                 """
                 INSERT INTO competitors (
@@ -87,25 +84,23 @@ def insert_competitors_query(data: dict) -> None:
                 """
             )
 
-            # competitors 데이터를 데이터베이스에 삽입
             for event in data['summaries']:
                 sport_event = event['sport_event']
-                for competitor in sport_event['competitors']:
+                for competitor in sport_event.get('competitors', []):
                     cursor.execute(insert_query, (
-                        competitor['id'],
-                        sport_event['id'],
-                        competitor['name'],
-                        competitor['country'],
-                        competitor['country_code'],
-                        competitor['abbreviation'],
-                        competitor['qualifier'],
-                        competitor['gender']
+                        competitor.get('id'),
+                        sport_event.get('id'),
+                        competitor.get('name'),
+                        competitor.get('country'),
+                        competitor.get('country_code'),
+                        competitor.get('abbreviation'),
+                        competitor.get('qualifier'),
+                        competitor.get('gender')
                     ))
                 connection.commit()
 
-
     except (Exception, psycopg2.Error) as error:
-        print("Error while inserting to PostgreSQL", error)
+        print("Error while inserting competitors to PostgreSQL:", error)
     finally:
         if connection:
             connection.close()
@@ -122,7 +117,6 @@ def insert_venue_query(data: dict) -> None:
         )
 
         with connection.cursor() as cursor:
-            # SQL 문 구성
             insert_query = sql.SQL(
                 """
                 INSERT INTO venue (
@@ -132,23 +126,21 @@ def insert_venue_query(data: dict) -> None:
                 """
             )
 
-            # competitors 데이터를 데이터베이스에 삽입
             for event in data['summaries']:
-                venue = event['sport_event']['venue']
+                venue = event['sport_event'].get('venue', {})
                 cursor.execute(insert_query, (
-                    venue['id'],
-                    venue['name'],
-                    venue['city_name'],
-                    venue['country_name'],
-                    venue['map_coordinates'],
-                    venue['country_code'],
-                    venue['timezone']
+                    venue.get('id'),
+                    venue.get('name'),
+                    venue.get('city_name'),
+                    venue.get('country_name'),
+                    venue.get('map_coordinates'),
+                    venue.get('country_code'),
+                    venue.get('timezone')
                 ))
             connection.commit()
 
-
     except (Exception, psycopg2.Error) as error:
-        print("Error while inserting to PostgreSQL", error)
+        print("Error while inserting venue to PostgreSQL:", error)
     finally:
         if connection:
             connection.close()
@@ -165,33 +157,30 @@ def insert_period_scores_query(data: dict) -> None:
         )
 
         with connection.cursor() as cursor:
-            # SQL 문 구성
             insert_query = sql.SQL(
                 """
                 INSERT INTO period_scores (
                     event_id, period_number, home_score, away_score, period_type
                 ) VALUES (%s, %s, %s, %s, %s)
-                ON CONFLICT (id) DO NOTHING
+                ON CONFLICT (event_id, period_number) DO NOTHING
                 """
             )
 
-            # competitors 데이터를 데이터베이스에 삽입
             for event in data['summaries']:
                 sport_event_id = event['sport_event']['id']
-                period_scores = event['sport_event_status']['period_scores']
+                period_scores = event['sport_event_status'].get('period_scores', [])
                 for period_score in period_scores:
                     cursor.execute(insert_query, (
                         sport_event_id,
-                        period_score['number'],
-                        period_score['home_score'],
-                        period_score['away_score'],
-                        period_score['type']
+                        period_score.get('number'),
+                        period_score.get('home_score'),
+                        period_score.get('away_score'),
+                        period_score.get('type')
                     ))
             connection.commit()
 
-
     except (Exception, psycopg2.Error) as error:
-        print("Error while inserting to PostgreSQL", error)
+        print("Error while inserting period_scores to PostgreSQL:", error)
     finally:
         if connection:
             connection.close()
