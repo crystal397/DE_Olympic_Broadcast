f6286b1e4bb659dbf18e07994d51bc7e868bc687
a26 <a26@a26.local>
2024-08-05 22:57:57 +0900

    배치 처리

---
+++ app/api_call/make_urn_list.py
@@ -0,0 +1,411 @@
+data=\
+  {
+  "generated_at": "2024-08-05T13:55:57+00:00",
+  "seasons": [
+    {
+      "id": "sr:season:33721",
+      "name": "Olympic Games 2016",
+      "start_date": "2016-08-05",
+      "end_date": "2016-08-20",
+      "year": "2016",
+      "competition_id": "sr:competition:551"
+    },
+    {
+      "id": "sr:season:84960",
+      "name": "Olympic Games 2021",
+      "start_date": "2021-07-24",
+      "end_date": "2021-08-07",
+      "year": "2021",
+      "competition_id": "sr:competition:551"
+    },
+    {
+      "id": "sr:season:105521",
+      "name": "Olympic Games 2024",
+      "start_date": "2024-07-27",
+      "end_date": "2024-08-10",
+      "year": "2024",
+      "competition_id": "sr:competition:551"
+    },
+    {
+      "id": "sr:season:33723",
+      "name": "Olympic Games Women 2016",
+      "start_date": "2016-08-05",
+      "end_date": "2016-08-12",
+      "year": "2016",
+      "competition_id": "sr:competition:552"
+    },
+    {
+      "id": "sr:season:84962",
+      "name": "Olympic Games Women 2021",
+      "start_date": "2021-07-24",
+      "end_date": "2021-08-06",
+      "year": "2021",
+      "competition_id": "sr:competition:552"
+    },
+    {
+      "id": "sr:season:105523",
+      "name": "Olympic Games Women 2024",
+      "start_date": "2024-07-27",
+      "end_date": "2024-08-09",
+      "year": "2024",
+      "competition_id": "sr:competition:552"
+    },
+    {
+      "id": "sr:season:86046",
+      "name": "CEV Europ. Championship 2021",
+      "start_date": "2021-08-12",
+      "end_date": "2021-08-16",
+      "year": "2021",
+      "competition_id": "sr:competition:1353"
+    },
+    {
+      "id": "sr:season:96971",
+      "name": "CEV Europ. Championship 2022",
+      "start_date": "2022-08-16",
+      "end_date": "2022-08-21",
+      "year": "2022",
+      "competition_id": "sr:competition:1353"
+    },
+    {
+      "id": "sr:season:107969",
+      "name": "CEV Europ. Championship 2023",
+      "start_date": "2023-08-02",
+      "end_date": "2023-08-06",
+      "year": "2023",
+      "competition_id": "sr:competition:1353"
+    },
+    {
+      "id": "sr:season:86044",
+      "name": "CEV Europ. Ch.ship, W. 2021",
+      "start_date": "2021-08-11",
+      "end_date": "2021-08-15",
+      "year": "2021",
+      "competition_id": "sr:competition:1355"
+    },
+    {
+      "id": "sr:season:96973",
+      "name": "CEV Europ. Ch.ship, W. 2022",
+      "start_date": "2022-08-15",
+      "end_date": "2022-08-20",
+      "year": "2022",
+      "competition_id": "sr:competition:1355"
+    },
+    {
+      "id": "sr:season:107973",
+      "name": "CEV Europ. Ch.ship, W. 2023",
+      "start_date": "2023-08-02",
+      "end_date": "2023-08-06",
+      "year": "2023",
+      "competition_id": "sr:competition:1355"
+    },
+    {
+      "id": "sr:season:67887",
+      "name": "FIVB World Championship 2019",
+      "start_date": "2019-06-28",
+      "end_date": "2019-07-08",
+      "year": "2019",
+      "competition_id": "sr:competition:2212"
+    },
+    {
+      "id": "sr:season:94231",
+      "name": "World Championship 2022",
+      "start_date": "2022-06-10",
+      "end_date": "2022-06-19",
+      "year": "2022",
+      "competition_id": "sr:competition:2212"
+    },
+    {
+      "id": "sr:season:111052",
+      "name": "World Championship 2023",
+      "start_date": "2023-10-06",
+      "end_date": "2023-10-15",
+      "year": "2023",
+      "competition_id": "sr:competition:2212"
+    },
+    {
+      "id": "sr:season:67893",
+      "name": "World Championship 2019, Women",
+      "start_date": "2019-06-28",
+      "end_date": "2019-07-08",
+      "year": "2019",
+      "competition_id": "sr:competition:2214"
+    },
+    {
+      "id": "sr:season:94237",
+      "name": "World Championship, Women 2022",
+      "start_date": "2022-06-10",
+      "end_date": "2022-06-19",
+      "year": "2022",
+      "competition_id": "sr:competition:2214"
+    },
+    {
+      "id": "sr:season:111054",
+      "name": "World Championship, Women 2023",
+      "start_date": "2023-10-06",
+      "end_date": "2023-10-15",
+      "year": "2023",
+      "competition_id": "sr:competition:2214"
+    },
+    {
+      "id": "sr:season:76179",
+      "name": "NCAA 2020, Women",
+      "start_date": "2020-02-20",
+      "end_date": "2020-05-04",
+      "year": "2020",
+      "competition_id": "sr:competition:24692"
+    },
+    {
+      "id": "sr:season:91371",
+      "name": "NCAA Women, Regular Season 2022",
+      "start_date": "2022-02-25",
+      "end_date": "2022-05-15",
+      "year": "2022",
+      "competition_id": "sr:competition:24692"
+    },
+    {
+      "id": "sr:season:102987",
+      "name": "NCAA, Women 2023",
+      "start_date": "2023-02-22",
+      "end_date": "2023-05-15",
+      "year": "2023",
+      "competition_id": "sr:competition:24692"
+    },
+    {
+      "id": "sr:season:55151",
+      "name": "San Francisco Open 2018",
+      "start_date": "2018-07-05",
+      "end_date": "2018-07-09",
+      "year": "2018",
+      "competition_id": "sr:competition:25967"
+    },
+    {
+      "id": "sr:season:54361",
+      "name": "New York City Open 2018",
+      "start_date": "2018-06-07",
+      "end_date": "2018-06-11",
+      "year": "2018",
+      "competition_id": "sr:competition:25971"
+    },
+    {
+      "id": "sr:season:67711",
+      "name": "New York City Open 2019",
+      "start_date": "2019-06-07",
+      "end_date": "2019-06-11",
+      "year": "2019",
+      "competition_id": "sr:competition:25971"
+    },
+    {
+      "id": "sr:season:54733",
+      "name": "Seattle Open 2018",
+      "start_date": "2018-06-21",
+      "end_date": "2018-06-25",
+      "year": "2018",
+      "competition_id": "sr:competition:25973"
+    },
+    {
+      "id": "sr:season:67881",
+      "name": "Seattle Open 2019",
+      "start_date": "2019-06-21",
+      "end_date": "2019-06-24",
+      "year": "2019",
+      "competition_id": "sr:competition:25973"
+    },
+    {
+      "id": "sr:season:53670",
+      "name": "Austin Open 2018",
+      "start_date": "2018-05-17",
+      "end_date": "2018-09-05",
+      "year": "2018",
+      "competition_id": "sr:competition:25977"
+    },
+    {
+      "id": "sr:season:66107",
+      "name": "Austin Open 2019",
+      "start_date": "2019-05-15",
+      "end_date": "2019-05-18",
+      "year": "2019",
+      "competition_id": "sr:competition:25977"
+    },
+    {
+      "id": "sr:season:54363",
+      "name": "New York City Open 2018, Women",
+      "start_date": "2018-06-07",
+      "end_date": "2018-06-11",
+      "year": "2018",
+      "competition_id": "sr:competition:25981"
+    },
+    {
+      "id": "sr:season:67713",
+      "name": "New York City Open 2019, Women",
+      "start_date": "2019-06-07",
+      "end_date": "2019-06-11",
+      "year": "2019",
+      "competition_id": "sr:competition:25981"
+    },
+    {
+      "id": "sr:season:53674",
+      "name": "Austin Open 2018, Women",
+      "start_date": "2018-05-17",
+      "end_date": "2018-05-23",
+      "year": "2018",
+      "competition_id": "sr:competition:25983"
+    },
+    {
+      "id": "sr:season:66109",
+      "name": "Austin Open 2019, Women",
+      "start_date": "2019-05-17",
+      "end_date": "2019-05-20",
+      "year": "2019",
+      "competition_id": "sr:competition:25983"
+    },
+    {
+      "id": "sr:season:56413",
+      "name": "Hermosa Beach Open 2018",
+      "start_date": "2018-07-26",
+      "end_date": "2018-07-30",
+      "year": "2018",
+      "competition_id": "sr:competition:25985"
+    },
+    {
+      "id": "sr:season:68324",
+      "name": "Hermosa Beach Open 2019",
+      "start_date": "2019-07-26",
+      "end_date": "2019-07-29",
+      "year": "2019",
+      "competition_id": "sr:competition:25985"
+    },
+    {
+      "id": "sr:season:56415",
+      "name": "Hermosa Beach Open 2018, Women",
+      "start_date": "2018-07-26",
+      "end_date": "2018-07-30",
+      "year": "2018",
+      "competition_id": "sr:competition:25987"
+    },
+    {
+      "id": "sr:season:68326",
+      "name": "Hermosa Beach Open 2019, Women",
+      "start_date": "2019-07-26",
+      "end_date": "2019-07-29",
+      "year": "2019",
+      "competition_id": "sr:competition:25987"
+    },
+    {
+      "id": "sr:season:56728",
+      "name": "Manhattan Beach Open 2018",
+      "start_date": "2018-08-17",
+      "end_date": "2018-08-20",
+      "year": "2018",
+      "competition_id": "sr:competition:26108"
+    },
+    {
+      "id": "sr:season:69836",
+      "name": "Manhattan Beach Open 2019",
+      "start_date": "2019-08-16",
+      "end_date": "2019-08-19",
+      "year": "2019",
+      "competition_id": "sr:competition:26108"
+    },
+    {
+      "id": "sr:season:56730",
+      "name": "Manhattan Beach Open 2018, Women",
+      "start_date": "2018-08-17",
+      "end_date": "2018-08-20",
+      "year": "2018",
+      "competition_id": "sr:competition:26110"
+    },
+    {
+      "id": "sr:season:69838",
+      "name": "Manhattan Beach Open 2019, Women",
+      "start_date": "2019-08-16",
+      "end_date": "2019-08-19",
+      "year": "2019",
+      "competition_id": "sr:competition:26110"
+    },
+    {
+      "id": "sr:season:57382",
+      "name": "The Championship, Chicago 2018",
+      "start_date": "2018-08-30",
+      "end_date": "2018-09-04",
+      "year": "2018",
+      "competition_id": "sr:competition:26368"
+    },
+    {
+      "id": "sr:season:70374",
+      "name": "The Championship, Chicago 2019",
+      "start_date": "2019-08-30",
+      "end_date": "2019-09-03",
+      "year": "2019",
+      "competition_id": "sr:competition:26368"
+    },
+    {
+      "id": "sr:season:57380",
+      "name": "The Championship, Chicago 2018, Women",
+      "start_date": "2018-08-30",
+      "end_date": "2018-09-04",
+      "year": "2018",
+      "competition_id": "sr:competition:26370"
+    },
+    {
+      "id": "sr:season:70376",
+      "name": "The Championship, Chicago 2019, Women",
+      "start_date": "2019-08-30",
+      "end_date": "2019-09-03",
+      "year": "2019",
+      "competition_id": "sr:competition:26370"
+    },
+    {
+      "id": "sr:season:57816",
+      "name": "Hawaii Invitational 2018",
+      "start_date": "2018-09-14",
+      "end_date": "2018-09-17",
+      "year": "2018",
+      "competition_id": "sr:competition:26502"
+    },
+    {
+      "id": "sr:season:57818",
+      "name": "Hawai Invitational 2018, Womeni",
+      "start_date": "2018-09-14",
+      "end_date": "2018-09-17",
+      "year": "2018",
+      "competition_id": "sr:competition:26504"
+    },
+    {
+      "id": "sr:season:67675",
+      "name": "Huntington Open 2019",
+      "start_date": "2019-05-02",
+      "end_date": "2019-05-06",
+      "year": "2019",
+      "competition_id": "sr:competition:29035"
+    },
+    {
+      "id": "sr:season:76635",
+      "name": "Huntington Open 2020",
+      "start_date": "2020-05-01",
+      "end_date": "2020-05-04",
+      "year": "2020",
+      "competition_id": "sr:competition:29035"
+    },
+    {
+      "id": "sr:season:67677",
+      "name": "Huntington Open 2019, Women",
+      "start_date": "2019-05-02",
+      "end_date": "2019-05-06",
+      "year": "2019",
+      "competition_id": "sr:competition:29037"
+    },
+    {
+      "id": "sr:season:76637",
+      "name": "Huntington Open 2020, Women",
+      "start_date": "2020-05-01",
+      "end_date": "2020-05-04",
+      "year": "2020",
+      "competition_id": "sr:competition:29037"
+    }
+  ]
+}
+
+ids = list()
+for x in data['seasons']:
+  ids.append(x['id'].replace(':', '%3A'))
+print(ids)
